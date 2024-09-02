c1953ee8be4da79f6654461261322263bb91985a
Adhitya Primandhika <adhityaprimandhika@gmail.com>
2024-09-02 22:03:38 +0700

    finish learning

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -3,6 +3,12 @@ import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
 import React from 'react';
+
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Dashboard | Invoice | Edit',
+};
  
 export default async function Page({ params }: { params: { id: string } }) {
     const id = params.id;  
